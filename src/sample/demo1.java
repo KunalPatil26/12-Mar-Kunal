be562350e612aa7062de9def427d3b8bc9b28e7b
Lenovo <Lenovo@LAPTOP-BKDTF7JP>
2022-07-19 20:55:16 +0530

    [Kunal]:add

---
+++ src/sample/demo1.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class demo1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hii");
+		
+		System.out.println("Kunal");
+		
+		System.out.println("Patil");
+		
+	}
+
+}
